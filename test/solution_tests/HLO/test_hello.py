5f0c6bbad6b0d7cb16035d6b806d0fc726662783
sbx_user1051 <sbx_user1051@169.254.7.125>
2022-06-26 11:48:18 +0000

    Sun Jun 26 11:48:18 UTC 2022

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -5,9 +5,11 @@ from solutions.HLO import hello
 @pytest.mark.parametrize(
     "friend_name,expected",
     [
-        ("Craftsman", "Hello, World!"),
-        ("Mr. X", "Hello, World!"),
+        ("Craftsman", "Hello, Craftsman!"),
+        ("Mr. X", "Hello, Mr. X!"),
+        ("John", "Hello, John!"),
     ]
 )
 def test_hello(friend_name, expected):
     assert hello(friend_name) == expected
+
