2ec6878228456d49d64251cf95377e6c4f273e34
sbx_user1051 <sbx_user1051@169.254.7.125>
2022-06-26 11:55:23 +0000

    Sun Jun 26 11:55:23 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,14 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    cost = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
+    try:
+        return sum([cost[sku] for sku in skus])
+    except KeyError:
+        return -1
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,5 +1,25 @@
+import pytest
 from solutions.CHK import checkout
 
 
-def test_checkout():
-    assert checkout("A") == 50
+@pytest.mark.parametrize(
+    "skus,price",
+    [
+        ("A", 50),
+        ("B", 30),
+        ("C", 20),
+        ("D", 15),
+        ("ABCD", 115),
+        ("E", -1),
+        ("AAA", 130),
+        ("AAAA", 180),
+        ("AAAAA", 230),
+        ("AAAAAA", 260),
+        ("BB", 45),
+        ("BBB", 75),
+        ("BBBB", 90),
+    ]
+)
+def test_checkout(skus, price):
+    assert checkout(skus) == price
+
